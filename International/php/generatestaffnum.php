bec551a008b625323149d77eb9d6465b00a30f4f
SOOHYEONCHOI <shychoi7@gmail.com>
2018-12-02 02:49:09 -0600

    ss
    ss

--- International/php/generatestaffnum.php
+++ International/php/generatestaffnum.php
@@ -1,4 +1,5 @@
 <?php
+  session_start();
   $username_d = $_POST["username"];
 
   $conn = mysqli_connect('localhost', 'root', 'asd123', 'drunkencode') or die("Failed");
@@ -7,10 +8,13 @@
   $u_data = mysqli_fetch_assoc($u_result);
 
   if($u_data['flag1'] == 1){
-    $s_sql = ""
+    $rand_number = rand(1111,9999);
+    $temp_staffNum = "ST"."-".$rand_number."-".$username_d;
 
-    echo "<script>alert(\"Welcome\");</script>";
-    echo("<script>location.replace('../index.html');</script>");
+    $sql = "UPDATE user_account SET staffNum = '$temp_staffNum' where username ='$username_d'";
+    mysqli_query($conn, $sql);
+    echo "<script>alert(\"$username_d's staff number is $temp_staffNum\");</script>";
+    echo("<script>location.replace('../created_staffnum.html');</script>");
   }
   else{
     echo "<script>alert(\"Invalid username, please check staff's user name\");</script>";
